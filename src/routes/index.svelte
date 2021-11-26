efaa567dc28126cde87a041809a28908a0dd599b
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2021-11-26 16:49:49 +0100

    prepare example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
+<script context="module">
+  export const ssr = false
+  export const prerender = true
+  export const hydrate = false
+
+  export async function load ({ page, fetch}) {
+    const markdown = await fetch("/HELLO.md")
+
+    return {
+      props: {
+        markdown: await markdown.text()
+      }
+    }
+  }
+</script>
+
+<script>
+  export let markdown
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<code>{markdown}</code>
---
+++ static/HELLO.md
@@ -0,0 +1,4 @@
+Hello World!
+
+Please make sure I was actually fetched at *compile time* or just at runtime by the client.
+If SSR is disabled the latter seems to always be happening
